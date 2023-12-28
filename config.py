2d9ae07551bad110be50891931a736806f058dc2
Wa <122730917+STKR2@users.noreply.github.com>
2023-12-28 15:36:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 API_ID = int(os.getenv("API_ID", "8934899"))
-CHAT_ID = int(os.getenv("CHAT_ID", "-1001590839214"))
-CHANNEL_USERNAMWE = os.getenv("CHANNEL_USERNAMWE", None)
+CHAT_ID = int(os.getenv("CHAT_ID", "-1001731985493"))
+CHANNEL_USERNAMWE = os.getenv("CHANNEL_USERNAMWE", "Pureyourheart")
 API_HASH = os.getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
 SESSION_STRING = os.getenv("SESSION_STRING", "BABxd8zopMXM5qWDm5DAFojCZvcZ7wedSgMSvRlVSrXPq9LG-AgbyF5my2ob5HcnVqRFXW2UWgvOLSJHPmBwxuxs85XocbCAFskfSsGuCdWhi7xXr7McbOfNJIWtTTuS2yTi51YGRi3Tn5VC_skBELKv16eNAYEQmB5XINMPyQrChKexTlim9VgdcO6CU9nPb-0UZ-QbXDMGHQYEH8poffAyyERknEkoSvpK4EbeuxDiKMh8bcx9NbEsrSOkWZE1ZZveF9CrjdXjbIc1nuzUPbAKp8AXAB_zHoNHGmaJUKWAF3zIRAvvkI11XTD5CZgBFZXnxUoeUkKo4aX-3wZCPz9DAAAAAXXf410A") # To Extract string_session just run "String.py"
